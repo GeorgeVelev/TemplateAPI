0ab311142918a1b101f2760dd092b239555e4e65
Aleksandar Kotrulev <aleksandar.kotrulev@scalefocus.com>
2020-02-28 16:15:01 +0200

    Add java doc to CustomAssert class

--- src/main/java/utility/CustomAssert.java
+++ src/main/java/utility/CustomAssert.java
@@ -3,6 +3,13 @@ package utility;
 import org.testng.Reporter;
 import org.testng.asserts.SoftAssert;
 
+/**
+ * This class enables us to display the logs for each test in the assertion error thrown.
+ * This is done so when running the tests in parallel, the proper logs are displayed in the proper
+ * test case tab, making for a better readability and html reports.
+ * <p>
+ * Note that with this, logs will be displayed only when a test fails
+ */
 public class CustomAssert extends SoftAssert {
     @Override
     public void assertAll() {
@@ -11,6 +18,11 @@ public class CustomAssert extends SoftAssert {
         super.assertAll(stringBuilder.toString());
     }
 
+    /**
+     * @param actual   the actual value to be asserted
+     * @param expected the expected value to be asserted
+     * @param <T>      the type of the variables. This makes it so we have a single implementation for all data types
+     */
     public static <T> void areEqual(final T actual, final T expected) {
         CustomAssert customAssert = new CustomAssert();
         customAssert.assertEquals(actual, expected);
